e5f6a617a0ceb537b19cadf0ff69099de1b537f8
Victor Alves <victorhugo_sp4@hotmail.com>
2022-02-07 17:58:06 -0300

    configuração das rotas para requisições

---
+++ config/routes.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const routes = express.Router();
+
+let db = [
+    'Augusto',
+    'Michele',
+    'Renato'
+];
+
+//enviar dados
+routes.get('/', (req, res) => {
+    return res.json(db);
+});
+
+//receber dados
+routes.post('/add', (req, res) => {
+    const body = req.body;
+
+    if(!body){
+        return res.status(400).end();
+    }
+
+    db.push(body.nome);
+    return res.json(db);
+});
+
+//apagar dados
+routes.delete('/:id', (req, res) => {
+    const id = req.params.id;
+
+    if(db[id] == undefined){
+	return res.json("Usuário não existe!");
+    }
+    db.splice(id, 1);
+    return res.json(db);
+});
+
+module.exports = routes;
